ca30dc94deba627b20e78d323b4a7ef162434cbc
Catalin Miron <catalinmiron093@gmail.com>
2025-07-15 19:10:27 +0300

    optimization

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,18 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+// export const metadata = {
+//   title: "Feed",
+//   description: "All posts by all users.",
+// };
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+  return {
+    title: `Feed of ${posts.length}`,
+    description: `All ${posts.length} posts by all users.`,
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,6 +1,11 @@
 import { handleSubmit } from "@/actions/posts";
 import PostForm from "@/components/postForm";
 
+export const metadata = {
+  title: "New Post",
+  description: "Create a new post to share with others.",
+};
+
 export default function NewPostPage() {
   return <PostForm action={handleSubmit} />;
 }
